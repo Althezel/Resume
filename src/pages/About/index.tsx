d63241678892ffa1aebf8fcabcd52b2c081e818d
Logan Whompton <loganc92vt@gmail.com>
2022-03-07 16:07:42 -0500

    Styling refactor and added contact info section

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,9 +1,44 @@
 import * as React from 'react';
 import Heading from 'components/Heading';
 
+import './styles.scss';
+
+const ContactInfoArticle = (): JSX.Element => (
+    <article>
+        <Heading level={2} content="Contact Information" />
+        <p>
+            <strong>Name: </strong>
+            Logan A. Whompton
+        </p>
+        <p className="link_container">
+            <strong>Github:</strong>
+            <a
+                href="https://www.github.com/Althezel"
+                rel="noreferrer noopener"
+                target="_blank"
+            >
+                www.github.com/Althezel
+            </a>
+        </p>
+        <p className="link_container">
+            <strong>LinkedIn:</strong>
+            <a
+                href="https://www.linkedin.com/in/logan-whompton"
+                rel="noreferrer noopener"
+                target="_blank"
+            >
+                www.linkedin.com/in/logan-whompton
+            </a>
+        </p>
+    </article>
+);
+
 const AboutPage = (): JSX.Element => (
     <main>
-        <Heading level={1} content="About Me" />
+        <section>
+            <Heading level={1} content="About" />
+            <ContactInfoArticle />
+        </section>
     </main>
 );
 
